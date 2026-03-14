13a74ba5c1ad9db635d95a1d442963f1e788fb84
Ty Loftin <tyloftin4@gmail.com>
2026-03-14 12:56:15 -0400

    messgae

--- simulation/environment.py
+++ simulation/environment.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from core.config_loader import PhysiologyConfig
 
-
+# This will be a lot more complex in the coming pushes
 def build_environment_state(physiology_config: PhysiologyConfig) -> dict[str, float]:
     return {
         "pH": physiology_config.pH,
